80c795461108bab6b812d2d739a22cdc5ce45a68
sashokrist <alexander.krist@gmail.com>
2022-02-07 11:09:12 +0200

    edit MeetSeeder.php

--- database/seeders/MeetSeeder.php
+++ database/seeders/MeetSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Meet;
+use App\Models\Room;
 use Illuminate\Database\Seeder;
 
 class MeetSeeder extends Seeder
@@ -17,12 +18,14 @@ class MeetSeeder extends Seeder
         Meet::truncate();
         $faker = \Faker\Factory::create();
         for ($i = 0; $i < 10; $i++) {
-            Meet::create([
-                             'name' => $faker->name,
-                             'room' => $faker->randomElement(['room 1', 'room 2', 'room 3']),
-                            'start_time' => $faker->dateTime,
-                            'end_time' => $faker->dateTime,
-                         ]);
+            Meet::create(['name' => $faker->name ]);
+            Room::create([
+                'meet_id' => $faker->numberBetween(1, 10),
+                'room_name' => $faker->randomElement(['room 1', 'room 2', 'room 3']),
+                'meet_date' => $faker->date(),
+                'start' => $faker->dateTime,
+                'end' => $faker->dateTime,
+            ]);
         }
     }
 }
